7ed99c6737c7568035c275042791c7efa69e5a7e
Riddhi73 <riddhimandebnath4260@gmail.com>
2023-05-21 12:57:24 +0600

    Create registration form

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,8 +5,8 @@ def home(request):
     context = {
         'posts':Post.objects.all()
     }
-    return render(request, 'blog\home.html', context)
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog\pageAbout.html', {'title':'About'})
+    return render(request, 'blog/about.html', {'title':'About'})
 
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
