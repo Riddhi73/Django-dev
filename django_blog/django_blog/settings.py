9e0af8ab92b28b4ba2e8f71df32d4f5cd99ed36d
Riddhi73 <riddhimandebnath4260@gmail.com>
2023-06-23 00:04:30 +0600

    Create directory to store profile pic

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -130,4 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'blog-login'
+
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 
